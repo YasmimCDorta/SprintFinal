c52e91017431eda6a06d57a8017667acfd5d5b6f
Yasmim Dorta <dortayasmim53@gmail.com>
2023-12-12 15:54:33 -0300

    Sprint Final

---
+++ Somativa/Somativa/Somativa/wwwroot/js/site.js
@@ -0,0 +1,8 @@
+﻿async function atualizaPreco() {
+    let id = document.getElementById("CodigoProd").value;
+    let Valor = document.getElementById("Valor");
+
+    await fetch(`/Produtos/Preco/${id}`)
+        .then(response => { return response.json(); })
+        .then(data => { Valor.value = data; })
+}
